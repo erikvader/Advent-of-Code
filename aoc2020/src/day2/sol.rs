5f7562f1ee3b2f6c3839765ac41b5ddf57fecd0d
Erik Rimskog <erik.rimskog@gmail.com>
2020-12-05 17:29:50 +0100

    updated day2

--- aoc2020/src/day2/sol.rs
+++ aoc2020/src/day2/sol.rs
@@ -2,18 +2,11 @@ use crate::parsers as P;
 use anyhow;
 
 pub fn part1(input: &str) -> anyhow::Result<String> {
-    let lines = P::list_of_regex_lines(input, r"^([0-9]+)-([0-9]+) ([a-zA-z]): ([a-zA-z]+)$")?;
+    let lines: Vec<(usize, usize, char, String)> =
+        P::list_of_regex_lines_parsed!(input, r"^([0-9]+)-([0-9]+) ([a-zA-z]): ([a-zA-z]+)$");
 
     let count = lines
         .into_iter()
-        .map(|x| {
-            (
-                x[0].parse::<usize>().unwrap(),
-                x[1].parse::<usize>().unwrap(),
-                x[2].parse::<char>().unwrap(),
-                x[3],
-            )
-        })
         .filter(|(lb, up, c, s)| {
             let count = s.chars().filter(|x| x == c).count();
             count >= *lb && count <= *up
@@ -24,18 +17,11 @@ pub fn part1(input: &str) -> anyhow::Result<String> {
 }
 
 pub fn part2(input: &str) -> anyhow::Result<String> {
-    let lines = P::list_of_regex_lines(input, r"^([0-9]+)-([0-9]+) ([a-zA-z]): ([a-zA-z]+)$")?;
+    let lines: Vec<(usize, usize, char, String)> =
+        P::list_of_regex_lines_parsed!(input, r"^([0-9]+)-([0-9]+) ([a-zA-z]): ([a-zA-z]+)$");
 
     let count = lines
         .into_iter()
-        .map(|x| {
-            (
-                x[0].parse::<usize>().unwrap(),
-                x[1].parse::<usize>().unwrap(),
-                x[2].parse::<char>().unwrap(),
-                x[3],
-            )
-        })
         .filter(|(lb, up, c, s)| {
             (s.as_bytes()[*lb - 1] as char == *c) ^ (s.as_bytes()[*up - 1] as char == *c)
         })
