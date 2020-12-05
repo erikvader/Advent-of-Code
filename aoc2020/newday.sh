1f971ecd8e6e7c537cf5e9f070fd21f54a259507
Erik Rimskog <erik.rimskog@gmail.com>
2020-12-05 18:24:17 +0100

    newday copies problem input from clipboard

--- aoc2020/newday.sh
+++ aoc2020/newday.sh
@@ -2,8 +2,13 @@
 
 set -e
 
+# add new module to main.rs
 sed -i -E '/^ *\/\/<day-marker>$/ i \
     mod day'"$1"';' src/main.rs
 
+# create the module file and folder
 mkdir src/day"$1"
 cp day_template.rs src/day"$1"/sol.rs
+
+# copy the problem input from the clipboard to an input file
+xclip -selection clipboard -o > src/day"$1"/input
