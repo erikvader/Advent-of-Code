85df4cc9a305e8653fe4d0c8916e714cfe1ab9e1
Erik Rimskog <erik.rimskog@gmail.com>
2020-12-06 21:55:32 +0100

    changed day_template

--- aoc2020/day_template.rs
+++ aoc2020/day_template.rs
@@ -2,9 +2,9 @@ use crate::parsers as P;
 use anyhow;
 
 pub fn part1(input: &str) -> anyhow::Result<String> {
-    unimplemented!();
+    Ok("part 1 is not implemented".to_string())
 }
 
 pub fn part2(input: &str) -> anyhow::Result<String> {
-    unimplemented!();
+    Ok("part 2 is not implemented".to_string())
 }
