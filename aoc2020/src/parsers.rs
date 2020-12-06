267c3333a784b565021a21f961ed8ac52e8363bf
Erik Rimskog <erik.rimskog@gmail.com>
2020-12-06 16:42:56 +0100

    removed Nom and changed to str::split

--- aoc2020/src/parsers.rs
+++ aoc2020/src/parsers.rs
@@ -1,65 +1,40 @@
 pub use crate::list_of_regex_lines_parsed;
 use grid::Grid;
-#[allow(unused_imports)]
-use nom::{
-    bytes::complete::tag,
-    character::complete::{char, digit1, none_of, one_of},
-    combinator::{all_consuming, cut, map_res, recognize},
-    error::{Error, ErrorKind},
-    multi::{many0, separated_list1},
-    regexp::str::re_capture,
-    Finish, IResult,
-};
 use regex::Regex;
 use std::convert::TryFrom;
 use std::str::FromStr;
 
-// list of parseable //////////////////////////////////////////////////////////
+// parser error ///////////////////////////////////////////////////////////////
 
 #[derive(Debug, thiserror::Error)]
 pub enum ParserError {
-    #[error("couldn't parse file because '{kind:?}'")]
-    Nom { kind: ErrorKind },
-    #[error("couldn't parse line '{line}' because '{parse}'")]
-    NomParse {
-        line: String,
-        kind: ErrorKind,
-        parse: Box<dyn std::error::Error + Send + Sync + 'static>,
-    },
     #[error(transparent)]
     Regex(#[from] regex::Error),
-    #[error("the input was empty")]
+    #[error("the regex didn't match")]
+    RegexNoMatch,
+    #[error("the input was empty or a capture group was None")]
     Empty,
+    #[error("a line failed to map because '{0}'")]
+    MapError(Box<dyn std::error::Error + Send + Sync + 'static>),
 }
 
-impl ParserError {
-    fn get_line(_input: &str) -> String {
-        // TODO: get the first line of input
-        String::new()
-    }
-}
+// list of parseable //////////////////////////////////////////////////////////
 
-impl nom::error::ParseError<&str> for ParserError {
-    fn from_error_kind(_input: &str, kind: ErrorKind) -> Self {
-        ParserError::Nom { kind: kind }
-    }
-
-    fn append(_input: &str, _kind: ErrorKind, other: Self) -> Self {
-        other
-    }
-}
-
-impl<E> nom::error::FromExternalError<&str, E> for ParserError
+pub fn map_sep<'a, F, T, E>(lines: &'a str, sep: &str, f: F) -> Result<Vec<T>, ParserError>
 where
+    F: Fn(&'a str) -> Result<T, E>,
     E: std::error::Error + Send + Sync + 'static,
 {
-    fn from_external_error(input: &str, kind: ErrorKind, e: E) -> Self {
-        ParserError::NomParse {
-            line: ParserError::get_line(input),
-            kind: kind,
-            parse: Box::new(e),
-        }
+    if lines.is_empty() {
+        return Err(ParserError::Empty);
     }
+
+    lines
+        .trim_end()
+        .split(sep)
+        .map(f)
+        .collect::<Result<_, _>>()
+        .map_err(|e| ParserError::MapError(Box::new(e)))
 }
 
 pub fn list_of_parseable<T, E>(s: &str, sep: &str) -> Result<Vec<T>, ParserError>
@@ -67,14 +42,7 @@ where
     T: std::str::FromStr<Err = E>,
     E: std::error::Error + Send + Sync + 'static,
 {
-    let r: IResult<&str, Vec<T>, ParserError> = all_consuming(separated_list1(
-        one_of(sep),
-        cut(map_res(recognize(many0(none_of(sep))), |x: &str| {
-            x.parse::<T>()
-        })),
-    ))(s);
-
-    r.finish().map(|x| x.1)
+    map_sep(s, sep, |x| x.parse())
 }
 
 pub fn list_of_parseable_lines<T, E>(s: &str) -> Result<Vec<T>, ParserError>
@@ -90,16 +58,15 @@ where
 pub fn list_of_regex_lines<'a>(s: &'a str, regex: &str) -> Result<Vec<Vec<&'a str>>, ParserError> {
     let reg = Regex::new(&format!("(?m){}", regex))?;
 
-    let r: IResult<&'a str, Vec<Vec<&'a str>>, ParserError> =
-        all_consuming(separated_list1(one_of("\n"), cut(re_capture(reg))))(s);
-
-    r.finish().map(|x| {
-        let mut lines = x.1;
-        lines.iter_mut().for_each(|l| {
-            l.remove(0);
-            ()
-        });
-        lines
+    map_sep(s, "\n", |l| {
+        reg.captures(l)
+            .and_then(|caps| {
+                caps.iter()
+                    .skip(1)
+                    .map(|g| g.map(|g| g.as_str()))
+                    .collect::<Option<Vec<&'a str>>>()
+            })
+            .ok_or(ParserError::RegexNoMatch)
     })
 }
 
