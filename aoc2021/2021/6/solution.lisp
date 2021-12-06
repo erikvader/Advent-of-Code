2924458394b6dd5d632f1fd7c002fc00bf21aa5e
Erik Rimskog <erik.rimskog@gmail.com>
2021-12-06 20:01:32 +0100

    day6

---
+++ aoc2021/2021/6/solution.lisp
@@ -0,0 +1,36 @@
+(ql:quickload "aoc")
+
+(defun shift-left (vec newvalue)
+  (let ((first (aref vec 0)))
+    (loop for i from 1 below (length vec)
+          do (setf (aref vec (1- i))
+                   (aref vec i)))
+    (setf (aref vec (1- (length vec)))
+          newvalue)
+    first))
+
+(defun calc-fishes (initial cycles)
+  (let ((fishes (loop with v = (make-array 9)
+                      for i from 0 to 8
+                      do (setf (aref v i)
+                               (count i initial))
+                      finally (return v))))
+    (dotimes (i cycles (reduce #'+ fishes))
+      (let ((zeros (shift-left fishes 0)))
+        (incf (aref fishes 8)
+              zeros)
+        (incf (aref fishes 6)
+              zeros)))))
+
+(defun part1 (numbers)
+  (calc-fishes numbers 80))
+
+(defun part2 (numbers)
+  (calc-fishes numbers 256))
+
+(defparameter *parser* (aoc:boll (aoc:each-line #'parse-integer) #'aoc:commas #'car))
+(aoc:run-day #'part1 :parser *parser*
+                     :expected-answer 379114)
+
+(aoc:run-day #'part2 :parser *parser*
+                     :expected-answer 1702631502303)
